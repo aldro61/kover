7db4cad070360c55da5114a85d8eeca27eaf541f
gletarte <gael.letarte-st-pierre.1@ulaval.ca>
2017-01-17 20:44:31 -0500

    wget to download gatb-core

--- core/kover/dataset/tools/kmer_tools/install.sh
+++ core/kover/dataset/tools/kmer_tools/install.sh
@@ -1,6 +1,20 @@
 # Remove any prior build
 rm -rf build
 
+#Removing gatb-core previous version
+cd thirdparty
+rm -rf gatb-core
+mkdir gatb-core
+cd gatb-core
+
+#Downloading gatb-core
+VERSION=v1.2.2
+URL=https://github.com/GATB/gatb-core/archive/$VERSION.tar.gz
+wget $URL
+tar -zxf $VERSION.tar.gz --strip-components=1
+rm $VERSION.tar.gz
+cd ../..
+
 # Create build directory
 mkdir  build
 cd build
