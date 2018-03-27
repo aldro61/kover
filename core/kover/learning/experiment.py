aldro61/kover

commit 2c4c75fb00c736ab356314cf4392e944d6eb125a

minor corrections

core/kover/learning/experiment.py
@@ -440,11 +440,11 @@ def learn(dataset_file, split_name, model_type, p, kmer_blacklist_file, max_rule
     p = np.unique(p)
 
     dataset = KoverDataset(dataset_file)
-
+    
+    # Find all rules to blacklist
     rule_blacklist = []
     if kmer_blacklist_file is not None:
         kmers_to_blacklist = _parse_blacklist(kmer_blacklist_file)
-        print(kmers_to_blacklist)
         if kmers_to_blacklist:
             kmer_sequences = (np.array(dataset.kmer_sequences)).tolist()
             kmer_by_rule = (np.array(dataset.kmer_by_matrix_column)).tolist()

core/kover/utils.py
@@ -130,6 +130,7 @@ def _unpack_binary_bytes_from_ints(a):
     return b
     
 def _parse_blacklist(blacklist_path):
+    # Fasta file formar
     if blacklist_path.endswith(".fasta"):
         with open(blacklist_path, "r") as fasta_file:
             data = fasta_file.read()
@@ -137,6 +138,7 @@ def _parse_blacklist(blacklist_path):
             data = [x for x in data if x]
             data= [(x.split('\n', 1))[1].rstrip('\n') for x in data]
             return data
+    # Other file format (one kmer per line)
     else:
         data = [l.rstrip('\n') for l in open(blacklist_path, "r")]
         data = [x for x in data if x]

interfaces/command_line.py
@@ -539,7 +539,7 @@ The most commonly used commands are:
                                  'more information.',
                             default=[0.1, 0.316, 0.562, 1.0, 1.778, 3.162, 10.0, 999999.0])
         parser.add_argument('--kmer-blacklist', help='A file containing a list of kmers to blacklist from being used to create rules.'
-                                                        'File format : either a fasta file or a text file with one kmer per line', required=False)
+                                                        'File format: either a fasta file or a text file with one kmer per line', required=False)
         parser.add_argument('--max-rules', type=int, help='The maximum number of rules that can be included in the '
                                                           'model.', default=10)
         parser.add_argument('--max-equiv-rules', type=int, help='The maximum number of equivalent rules to report for '

commit 388af2195f6adeec64e9ece9b58433136ae83b6b

Fixed a bug that was introduced in commit 2c4c75f
Incorrectly named argument that led to an exception when trying to use bound selection to selection hyperparameter values.

core/kover/learning/experiment.py
@@ -493,7 +493,7 @@ def learn(dataset_file, split_name, model_type, p, kmer_blacklist_file, max_rule
         best_hp, \
         best_model, \
         best_rule_importances, \
-        best_predictor_equiv_rules = _bound_selection(dataset_file=dataset_file, split_name=split_name, model_type=model_type,
+        best_predictor_equiv_rules = _bound_selection(dataset_file=dataset_file, split_name=split_name, model_types=model_type,
                                                       p_values=p, max_rules=max_rules, max_equiv_rules=max_equiv_rules,
                                                       rule_blacklist=rule_blacklist, bound_delta=bound_delta,
                                                       bound_max_genome_size=bound_max_genome_size, n_cpu=n_cpu,