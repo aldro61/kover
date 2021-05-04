2bdb9d9d1d500b6ea1b992e34c0a79911722412d
Alexandre Drouin <alexandre.drouin1@gmail.com>
2021-05-04 16:34:42 -0400

    Removed make -j8 since it sometimes causes mem errors in the docker testing environment

--- core/kover/dataset/tools/kmer_tools/install.sh
+++ core/kover/dataset/tools/kmer_tools/install.sh
@@ -11,10 +11,10 @@ cd gatb-core
 echo '* Downloading gatb-core'
 case "$(python --version 2>&1)" in
     *" 2."*)
-        python -c 'import urllib; urllib.urlretrieve ("https://github.com/GATB/gatb-core/archive/v1.2.2.tar.gz", "gatbcore.tar.gz")'
+        python -c 'import urllib; urllib.urlretrieve ("https://github.com/GATB/gatb-core/archive/v1.4.2.tar.gz", "gatbcore.tar.gz")'
         ;;
 	*" 3."*)
-		python -c 'import urllib.request; urllib.request.urlretrieve ("https://github.com/GATB/gatb-core/archive/v1.2.2.tar.gz", "gatbcore.tar.gz")'
+		python -c 'import urllib.request; urllib.request.urlretrieve ("https://github.com/GATB/gatb-core/archive/v1.4.2.tar.gz", "gatbcore.tar.gz")'
 	;;
     *)
         echo "Error: Python 2 or 3 is not installed"
@@ -32,7 +32,7 @@ echo '* Compiling gatb-core'
 cmake .. -Wno-dev
 
 # Run the makefile to build dsk, multidsk and dsk2kover tools:
-make -j8
+make
 
 # Clean up gatb-core source files
 echo '* Cleaning up gatb-core source files'
